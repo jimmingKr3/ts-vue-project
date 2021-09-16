aac8659f5e318e2e1aaa569af6981c61f69f944c
Jimming_kR3 <624476878@qq.com>
2021-09-16 15:21:51 +0800

    2021.9.16

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/view/login/index.vue
@@ -0,0 +1,5 @@
+<template><div class="father">Login</div></template>
+
+<script setup></script>
+
+<style scoped></style>
